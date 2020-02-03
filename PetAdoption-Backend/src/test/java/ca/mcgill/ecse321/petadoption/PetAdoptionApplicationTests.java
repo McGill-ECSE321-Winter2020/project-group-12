033a0009f665706eae5279098854dd752aa0485a
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2020-02-03 16:25:45 -0500

    build system

---
+++ PetAdoption-Backend/src/test/java/ca/mcgill/ecse321/petadoption/PetAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.petadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
