598f2840a662769dd7fb4dba68d61a704674d3cc
zakessad <zakaria.essadaoui@mail.mcgill.ca>
2020-02-06 14:41:15 -0500

    Refactor model.

--- PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/model/AppUser.java
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/model/AppUser.java
@@ -8,102 +8,102 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class AppUser{
-private Set<Donation> donations;
-   
-   @OneToMany(mappedBy="donor" )
-   public Set<Donation> getDonations() {
+    private Set<Donation> donations;
+
+    @OneToMany(mappedBy="donor" )
+    public Set<Donation> getDonations() {
       return this.donations;
-   }
-   
-   public void setDonations(Set<Donation> donationss) {
-      this.donations = donationss;
-   }
-   
-   private String name;
+    }
 
-public void setName(String value) {
+    public void setDonations(Set<Donation> donations) {
+      this.donations = donations;
+    }
+
+    private String name;
+
+    public void setName(String value) {
     this.name = value;
-}
-public String getName() {
+    }
+    public String getName() {
     return this.name;
-}
-private String email;
+    }
+    private String email;
 
-public void setEmail(String value) {
+    public void setEmail(String value) {
     this.email = value;
-}
-@Id
-public String getEmail() {
+    }
+    @Id
+    public String getEmail() {
     return this.email;
-}
-private String password;
+    }
+    private String password;
 
-public void setPassword(String value) {
+    public void setPassword(String value) {
     this.password = value;
-}
-public String getPassword() {
+    }
+    public String getPassword() {
     return this.password;
-}
-private String biography;
+    }
+    private String biography;
 
-public void setBiography(String value) {
+    public void setBiography(String value) {
     this.biography = value;
-}
-public String getBiography() {
+    }
+    public String getBiography() {
     return this.biography;
-}
-private String homeDescription;
+    }
+    private String homeDescription;
 
-public void setHomeDescription(String value) {
+    public void setHomeDescription(String value) {
     this.homeDescription = value;
-}
-public String getHomeDescription() {
+    }
+    public String getHomeDescription() {
     return this.homeDescription;
-}
-private Integer age;
+    }
+    private Integer age;
 
-public void setAge(Integer value) {
+    public void setAge(Integer value) {
     this.age = value;
-}
-public Integer getAge() {
+    }
+    public Integer getAge() {
     return this.age;
-}
-private Sex sex;
+    }
+    private Sex sex;
 
-public void setSex(Sex value) {
+    public void setSex(Sex value) {
     this.sex = value;
-}
-public Sex getSex() {
+    }
+    public Sex getSex() {
     return this.sex;
-}
-private boolean isAdmin;
+    }
+    private boolean isAdmin;
 
-public void setIsAdmin(boolean value) {
+    public void setIsAdmin(boolean value) {
     this.isAdmin = value;
-}
-public boolean isIsAdmin() {
+    }
+    public boolean isIsAdmin() {
     return this.isAdmin;
-}
-   private Set<Advertisement> advertisements;
-   
-   @OneToMany(mappedBy="postedBy" )
-   public Set<Advertisement> getAdvertisements() {
+    }
+    private Set<Advertisement> advertisements;
+
+    @OneToMany(mappedBy="postedBy" )
+    public Set<Advertisement> getAdvertisements() {
       return this.advertisements;
-   }
-   
-   public void setAdvertisements(Set<Advertisement> advertisementss) {
-      this.advertisements = advertisementss;
-   }
-   
-   private Set<Application> applications;
-   
-   @OneToMany(mappedBy="applicant" )
-   public Set<Application> getApplications() {
+    }
+
+    public void setAdvertisements(Set<Advertisement> advertisements) {
+      this.advertisements = advertisements;
+    }
+
+    private Set<Application> applications;
+
+    @OneToMany(mappedBy="applicant" )
+    public Set<Application> getApplications() {
       return this.applications;
-   }
-   
-   public void setApplications(Set<Application> applicationss) {
-      this.applications = applicationss;
-   }
-   
-   }
+    }
+
+    public void setApplications(Set<Application> applications) {
+      this.applications = applications;
+    }
+
+}
