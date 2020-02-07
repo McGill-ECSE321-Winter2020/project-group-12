7150c997c6ea8b4cd67e14b1d82d5616d47a7ad6
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2020-02-07 17:55:15 -0500

    Correct persistence test

--- PetAdoption-Backend/src/test/java/ca/mcgill/ecse321/petadoption/util/TestPetAdoptionPersistence.java
+++ PetAdoption-Backend/src/test/java/ca/mcgill/ecse321/petadoption/util/TestPetAdoptionPersistence.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.petadoption.util;
 
+import static java.lang.Boolean.FALSE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -10,7 +11,9 @@ import java.time.LocalTime;
 import java.time.Month;
 
 import ca.mcgill.ecse321.petadoption.dao.*;
+import ca.mcgill.ecse321.petadoption.service.PetAdoptionService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +32,9 @@ import ca.mcgill.ecse321.petadoption.model.Status;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestPetAdoptionPersistence {
+
+    @Autowired
+    private PetAdoptionService service;
     @Autowired
     private AdvertisementRepository advertisementRepository;
     @Autowired
@@ -40,7 +46,7 @@ public class TestPetAdoptionPersistence {
     @Autowired
     private ImageRepository imageRepository;
 
-    @AfterEach
+    @BeforeEach
     public void clearDatabase() {
         // First, we clear advertisement to avoid exceptions due to inconsistencies
         advertisementRepository.deleteAll();
@@ -54,24 +60,48 @@ public class TestPetAdoptionPersistence {
     @Test
     public void testPersistAndLoadAdvertisement() {
         Advertisement newad = new Advertisement();
+        newad.setDatePosted(new Date(12345));
+        newad.setPetDescription("lovely,cute,adorable");
+        newad.setPetAge(88);
+        newad.setPetName("Mojo");
+        newad.setIsExpired(false);
+
         advertisementRepository.save(newad);
         Long id = newad.getAdvertisementId();
         newad = null;
         newad = advertisementRepository.findAdvertisementByAdvertisementId(id);
         assertNotNull(newad);
-        assertEquals(id , newad.getAdvertisementId());
-//        // First example for object save/load
-//        Person person = new Person();
-//        // First example for attribute save/load
-//        person.setName(name);
-//        personRepository.save(person);
-//
-//        person = null;
-//
-//        person = personRepository.findPersonByName(name);
-//        assertNotNull(person);
-//        assertEquals(name, person.getName());
+        assertEquals(new Date(12345),newad.getDatePosted());
+        assertEquals("lovely,cute,adorable",newad.getPetDescription());
+        assertEquals(88,newad.getPetAge());
+        assertEquals("Mojo",newad.getPetName());
+        assertEquals(false,newad.isIsExpired());
+
     }
+
+//    @Test
+//    public void testPersistAndLoadEvent() {
+//        String name = "ECSE321 Tutorial";
+//        Date date = java.sql.Date.valueOf(LocalDate.of(2020, Month.JANUARY, 31));
+//        Time startTime = java.sql.Time.valueOf(LocalTime.of(11, 35));
+//        Time endTime = java.sql.Time.valueOf(LocalTime.of(13, 25));
+//        Event event = new Event();
+//        event.setName(name);
+//        event.setDate(date);
+//        event.setStartTime(startTime);
+//        event.setEndTime(endTime);
+//        eventRepository.save(event);
+//
+//        event = null;
+//
+//        event = eventRepository.findEventByName(name);
+//
+//        assertNotNull(event);
+//        assertEquals(name, event.getName());
+//        assertEquals(date, event.getDate());
+//        assertEquals(startTime, event.getStartTime());
+//        assertEquals(endTime, event.getEndTime());
+//    }
 
 
 }
