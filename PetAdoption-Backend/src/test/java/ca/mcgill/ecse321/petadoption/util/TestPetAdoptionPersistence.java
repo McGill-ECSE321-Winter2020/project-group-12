c072b100c4c87d6f05a36cbe77c405e456b9d042
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2020-02-07 12:19:45 -0500

    add persistence test

---
+++ PetAdoption-Backend/src/test/java/ca/mcgill/ecse321/petadoption/util/TestPetAdoptionPersistence.java
@@ -0,0 +1,45 @@
+package ca.mcgill.ecse321.petadoption.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+import ca.mcgill.ecse321.petadoption.dao.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.petadoption.model.Advertisement;
+import ca.mcgill.ecse321.petadoption.model.Application;
+import ca.mcgill.ecse321.petadoption.model.AppUser;
+import ca.mcgill.ecse321.petadoption.model.Donation;
+import ca.mcgill.ecse321.petadoption.model.Image;
+import ca.mcgill.ecse321.petadoption.model.Sex;
+import ca.mcgill.ecse321.petadoption.model.Species;
+import ca.mcgill.ecse321.petadoption.model.Status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestPetAdoptionPersistence {
+    @Autowired
+    private AdvertisementRepository advertisementRepository;
+    @Autowired
+    private ApplicationRepository applicationRepository;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private DonationRepository donationRepository;
+    @Autowired
+    private ImageRepository imageRepository;
+
+
+
+}
