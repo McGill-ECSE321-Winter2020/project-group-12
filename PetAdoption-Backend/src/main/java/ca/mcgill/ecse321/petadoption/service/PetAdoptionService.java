e223336abfeb8c36d2770d413fbb04e0125e50d1
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2020-02-15 13:50:08 -0500

    Fix this for Christina

---
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/service/PetAdoptionService.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.petadoption.service;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.petadoption.dao.AppUserRepository;
+import ca.mcgill.ecse321.petadoption.dao.AdvertisementRepository;
+import ca.mcgill.ecse321.petadoption.dao.ApplicationRepository;
+import ca.mcgill.ecse321.petadoption.dao.DonationRepository;
+import ca.mcgill.ecse321.petadoption.dao.ImageRepository;
+
+public class PetAdoptionService {
+
+}
