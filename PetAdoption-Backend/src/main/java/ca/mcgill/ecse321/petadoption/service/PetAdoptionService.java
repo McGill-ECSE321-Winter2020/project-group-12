e8734851185b6a36048a9b08f6557a5bd55e1c09
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2020-02-18 17:02:22 -0500

    Add service method

--- PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/service/PetAdoptionService.java
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/service/PetAdoptionService.java
@@ -30,6 +30,7 @@ public class PetAdoptionService {
     /**
      * Creates and adds a new Advertisement object to the database.
      *
+     * @param id
      * @param datePosted
      * @param isExpired
      * @param petName
@@ -40,9 +41,12 @@ public class PetAdoptionService {
      * @return Advertisement object
      */
     @Transactional
-    public Advertisement createAdvertisement(Date datePosted, boolean isExpired, String petName, Integer petAge, String petDescription, Sex petSex, Species petSpecies) {
+    public Advertisement createAdvertisement(String id, Date datePosted, boolean isExpired, String petName, Integer petAge, String petDescription, Sex petSex, Species petSpecies) {
         Advertisement ad = new Advertisement();
         String error = "";
+        if (id == null || id.trim().length() == 0) {
+            error = error + "id is not valid! ";
+        }
         if (datePosted == null) {
             error = "datePosted can not be empty! ";
         }
@@ -70,7 +74,7 @@ public class PetAdoptionService {
         }
 
         ad.setDatePosted(datePosted);
-        ad.setAdvertisementId();
+        ad.setAdvertisementId(id);
         ad.setPetDescription(petDescription);
         ad.setPetName(petName);
         ad.setPetAge(petAge);
@@ -84,15 +88,19 @@ public class PetAdoptionService {
     /**
      * Creates and adds a new Application object to the database.
      *
+     * @param id
      * @param dateOfSubmission
      * @param note
      * @param status
      * @return Application object
      */
     @Transactional
-    public Application createApplication(Date dateOfSubmission, String note, Status status) {
+    public Application createApplication(String id, Date dateOfSubmission, String note, Status status) {
         Application app = new Application();
         String error = "";
+        if (id == null || id.trim().length() == 0) {
+            error = error + "id is not valid! ";
+        }
         if (dateOfSubmission == null) {
             error = "dateOfSubmission can not be empty! ";
         }
@@ -107,13 +115,69 @@ public class PetAdoptionService {
             throw new IllegalArgumentException(error);
         }
 
-        app.setApplicationId();
+        app.setApplicationId(id);
         app.setDateOfSubmission(dateOfSubmission);
         app.setNote(note);
         app.setStatus(status);
 
         applicationRepository.save(app);
         return app;
+    }
+
+    /**
+     * Creates and adds a new AppUser object to the database.
+     *
+     * @param name(String)
+     * @param email(String)
+     * @param password(String)
+     * @param biography(String)
+     * @param homeDescription(String)
+     * @param age(Integer)
+     * @param isAdmin(boolean)
+     * @param sex(Sex)
+     * @return AppUser object
+     */
+    @Transactional
+    public AppUser createAppUser(String name, String email, String password, String biography, String homeDescription, Integer age, boolean isAdmin, Sex sex ) {
+        AppUser user1 = new AppUser();
+        String error = "";
+        if (name == null || name.trim().length() == 0) {
+            error = "name can not be empty! ";
+        }
+        if (email == null || email.trim().length() == 0) {
+            error = error + "email cannot be empty ";
+        }
+        if (password == null || password.trim().length() == 0) {
+            error = error + "password cannot be empty ";
+        }
+        if (biography == null || biography.trim().length() == 0) {
+            error = error + "biography cannot be empty ";
+        }
+        if (homeDescription == null || homeDescription.trim().length() == 0) {
+            error = error + "homeDescription cannot be empty ";
+        }
+        if (age <= 0) {
+            error = error + "age is not valid ";
+        }
+        if (sex != Sex.F && sex != Sex.M) {
+            error = error + "sex is invalid ";
+        }
+
+        if (error.length() != 0) {
+            throw new IllegalArgumentException(error);
+        }
+
+        user1.setSex(sex);
+        user1.setPassword(password);
+        user1.setIsAdmin(isAdmin);
+        user1.setHomeDescription(homeDescription);
+        user1.setBiography(biography);
+        user1.setAge(age);
+        user1.setEmail(email);
+        user1.setName(name);
+
+        appUserRepository.save(user1);
+        return user1;
     }
 
 }
