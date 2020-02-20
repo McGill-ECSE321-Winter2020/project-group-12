10cc61437b7d12a8f7e9daa5708f5ed897b89b43
Abboudi360 <aboudi.hassan@gmail.com>
2020-02-19 23:49:33 -0500

    Created dto and controller classes for application

---
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.petadoption.controller;
+
+import ca.mcgill.ecse321.petadoption.service.PetAdoptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class ApplicationController {
+    @Autowired
+    private PetAdoptionService service;
+
+    
+}
