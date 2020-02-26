f3a4ab430cda29a2e62276a252e37bb0209f205e
criachi <christina.riachi@mail.mcgill.ca>
2020-02-26 17:27:32 -0500

    changes

--- PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/controller/DonationController.java
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/controller/DonationController.java
@@ -1,22 +1,28 @@
 package ca.mcgill.ecse321.petadoption.controller;
 
+import ca.mcgill.ecse321.petadoption.model.Donation;
 import ca.mcgill.ecse321.petadoption.service.PetAdoptionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@Controller
 public class DonationController {
 
     @Autowired
     private PetAdoptionService service;
-//
-//    @GetMapping(value = {"/donations", "/donations/" })
-//    public ResponseEntity<List<Donation>> getAllDonations() {
-//        List<Donation> donations = service.getAllDonations();
-//        return new ResponseEntity(donations, HttpStatus.valueOf(200));
-//    }
+
+    @GetMapping(value = {"/donations", "/donations/" })
+    public ResponseEntity<List<Donation>> getAllDonations() {
+        List<Donation> donations = service.getAllDonations();
+        return new ResponseEntity(donations, HttpStatus.valueOf(200));
+    }
+
+
 }
