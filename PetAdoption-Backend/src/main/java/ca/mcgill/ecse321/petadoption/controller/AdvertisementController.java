8a419ce6d0e58f1c936d11cc64d993a886d1e003
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2020-02-27 22:40:12 -0500

    Implement Get mapping methods in advertisement controller

--- PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/controller/AdvertisementController.java
+++ PetAdoption-Backend/src/main/java/ca/mcgill/ecse321/petadoption/controller/AdvertisementController.java
@@ -4,8 +4,8 @@ import ca.mcgill.ecse321.petadoption.dto.AdvertisementDto;
 import ca.mcgill.ecse321.petadoption.model.Advertisement;
 import ca.mcgill.ecse321.petadoption.model.AppUser;
 import ca.mcgill.ecse321.petadoption.service.AdvertisementService;
+import ca.mcgill.ecse321.petadoption.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
@@ -17,31 +17,44 @@ import java.util.List;
 public class AdvertisementController {
 
     @Autowired
-    private AdvertisementService service;
+    private AdvertisementService advertisementService;
+    @Autowired
+    private AppUserService appUserService;
 
     //TODO: Test POST mapping for advertisement after implementing POST method for AppUser
     @PostMapping(value = {"{userId}/advertisement/create", "{userId}/advertisement/create/"})
-    public AdvertisementDto createAdvertisement(@RequestBody Advertisement ad, @RequestParam Date date,
+    public AdvertisementDto createAdvertisement(@RequestBody AdvertisementDto ad, @RequestParam Date date,
                                                 @PathVariable("userId") String userEmail) throws IllegalArgumentException {
-        Advertisement advertisement = service.createAdvertisement(userEmail, date, false, ad.getPetName(),
+        Advertisement advertisement = advertisementService.createAdvertisement(userEmail, date, ad.getPetName(),
                 ad.getPetAge(), ad.getPetDescription(), ad.getPetSex(), ad.getPetSpecies());
         return convertToDto(advertisement);
     }
 
     @PutMapping(value = {"/advertisement/update", "/advertisement/update/"})
-    public ResponseEntity<Object> updateAdvertisement(@RequestParam("adId") String advertisementID) {
-        return null;
+    public AdvertisementDto updateAdvertisementDetails(@RequestParam("adId") String advertisementID,
+                                                       @RequestBody AdvertisementDto advertisementDto) {
+        Advertisement advertisement = advertisementService.updateAdvertisement(advertisementID,
+                advertisementDto.getPetName(), advertisementDto.getPetAge(), advertisementDto.getPetDescription(),
+                advertisementDto.getPetSex(), advertisementDto.getPetSpecies());
+        return convertToDto(advertisement);
+    }
+
+    @PutMapping(value = {"/advertisement/updateExpiry", "/advertisement/updateExpiry/"})
+    public AdvertisementDto updateAdvertisementExpiration(@RequestParam("adId") String advertisementID,
+                                                          @RequestParam("expired") boolean expired){
+        Advertisement advertisement = advertisementService.updateAdvertisementIsExpired(advertisementID, expired);
+        return convertToDto(advertisement);
     }
 
     @DeleteMapping(value = {"/advertisement/delete", "/advertisement/delete/"})
     public boolean deleteAdvertisement(@RequestParam("adId") String advertisementID) {
-        return service.deleteAdvertisement(advertisementID);
+        return advertisementService.deleteAdvertisement(advertisementID);
     }
 
     @GetMapping(value = {"/advertisements", "/advertisements/"})
     public List<AdvertisementDto> getAllAdvertisements() {
         List<AdvertisementDto> advertisementDtoList = new ArrayList<>();
-        for (Advertisement advertisement : service.getAllAdvertisements()) {
+        for (Advertisement advertisement : advertisementService.getAllAdvertisements()) {
             advertisementDtoList.add(convertToDto(advertisement));
         }
         return advertisementDtoList;
@@ -50,22 +63,20 @@ public class AdvertisementController {
     //TODO: Test GET Mapping after AppUser class has been implemented
     @GetMapping(value = {"/{userID}/advertisements", "/{userID}/advertisements/"})
     public List<AdvertisementDto> getAdvertisementsByAppUser(@PathVariable("userID") String userEmail) {
-        List<Advertisement> advertisements;
         try {
-            AppUser appUser = service.getAppUserByEmail(userEmail);
+            AppUser appUser = appUserService.getAppUserByEmail(userEmail);
+            return createAdvertisementDtosForAppUser(appUser);
             // Converting HashSet of Advertisements to ArrayList of Advertisements
-            advertisements = new ArrayList<>(appUser.getAdvertisements());
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
-        return null;
     }
 
     @GetMapping(value = {"/advertisement/{advertisementID}", "/advertisement/{advertisementID/"})
     public AdvertisementDto getAdvertisementById(@PathVariable("advertisementID") String advertisementID) {
         Advertisement advertisement;
         try {
-            advertisement = service.getAdvertisementByID(advertisementID);
+            advertisement = advertisementService.getAdvertisementByID(advertisementID);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
@@ -78,12 +89,19 @@ public class AdvertisementController {
         if (ad == null) {
             throw new IllegalArgumentException("There is no such Advertisement!");
         }
-        AdvertisementDto advertisementDto = new AdvertisementDto(ad.getPostedBy(), ad.getDatePosted(),
+        return new AdvertisementDto(ad.getPostedBy().getEmail(), ad.getDatePosted(),
                 ad.getAdvertisementId(), ad.isIsExpired(), ad.getPetName(), ad.getPetAge(), ad.getPetDescription(),
                 ad.getPetSex(), ad.getPetSpecies(), ad.getApplications(), ad.getPetImages());
-        return advertisementDto;
     }
 
+    private List<AdvertisementDto> createAdvertisementDtosForAppUser(AppUser appUser) {
+        List<Advertisement> advertisementsForAppUser = advertisementService.getAdvertisementsOfAppUser(appUser);
+        List<AdvertisementDto> advertisementDtos = new ArrayList<>();
+        for (Advertisement advertisement : advertisementsForAppUser){
+            advertisementDtos.add(convertToDto(advertisement));
+        }
+        return advertisementDtos;
+    }
 //    Evaluate the necessities of the following convert to DTO methods for the Advertisement class
 //    Method needs convertToDto from application class
 //    public static List<ApplicationDto> createApplicationDtosForAdvertisement(Set<Application> applications){ Set or List?
